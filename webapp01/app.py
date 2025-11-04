95d761a00bd0582e88300c750aab651d3bac36ba
tony <tony.stark@MngEnvMCAP828289.onmicrosoft.com>
2025-11-04 08:44:29 +0000

    good

--- webapp01/app.py
+++ webapp01/app.py
@@ -29,4 +29,4 @@ def home():
     return render_template("index.html", now=now, temp=temp, facts=facts)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
